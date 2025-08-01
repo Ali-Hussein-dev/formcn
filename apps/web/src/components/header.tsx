322877e516689a66646b49e8c1ec03ef77364860
Ali <ali.hussein.pre@gmail.com>
2025-08-01 15:35:06 +0200

    initial commit

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,26 +1,30 @@
 "use client";
 import Link from "next/link";
-
 import { ModeToggle } from "./mode-toggle";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 export default function Header() {
   const links = [
-    { to: "/", label: "Home" },
+    { href: "https://github.com/Ali-Hussein-dev/formcn", Icon: FaGithub },
+    { href: "https://x.com/alibey_10", Icon: FaXTwitter },
   ];
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
-          {links.map(({ to, label }) => {
-            return (
-              <Link key={to} href={to}>
-                {label}
-              </Link>
-            );
-          })}
-        </nav>
+        <div>
+          <Link href="/">formcn</Link>
+        </div>
         <div className="flex items-center gap-2">
+          <nav className="flex gap-4 text-lg">
+            {links.map(({ href, Icon }) => {
+              return (
+                <a key={href} href={href}>
+                  <Icon />
+                </a>
+              );
+            })}
+          </nav>
           <ModeToggle />
         </div>
       </div>
