57fa4ddd96754c8c5ddc7ead940420b21dc17a3c
Ali <ali.hussein.pre@gmail.com>
2025-08-01 16:14:27 +0200

    update header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
+import { Logo } from "./shared/logo";
 
 export default function Header() {
   const links = [
@@ -13,13 +14,20 @@ export default function Header() {
     <div>
       <div className="flex flex-row items-center justify-between px-2 py-1">
         <div>
-          <Link href="/">formcn</Link>
+          <Link href="/" className="cursor-pointer aspect-video">
+            <Logo />
+          </Link>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center sm:gap-4 gap-2">
           <nav className="flex gap-4 text-lg">
             {links.map(({ href, Icon }) => {
               return (
-                <a key={href} href={href}>
+                <a
+                  key={href}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <Icon />
                 </a>
               );
