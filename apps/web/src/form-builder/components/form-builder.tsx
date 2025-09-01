1f77b5c9299acff07116a11aaee69889caefca8e
Ali <ali.hussein.pre@gmail.com>
2025-09-01 08:01:00 +0200

    fix: bring back the save button

--- apps/web/src/form-builder/components/form-builder.tsx
+++ apps/web/src/form-builder/components/form-builder.tsx
@@ -42,9 +42,17 @@ export function FormBuilder() {
   const formElements = useFormBuilderStore((s) => s.formElements);
   const isMS = useFormBuilderStore((s) => s.isMS);
   const searchParams = useSearchParams();
-
+  const getFormById = useLocalForms((s) => s.getFormById);
+  // const updateForm = useLocalForms((s) => s.updateForm);
   const id = searchParams.get("id");
+  // const foundform = getFormById(id!);
+  const saveForm = useLocalForms((s) => s.updateForm);
 
+  function handleSaveForm() {
+    if (!id) return;
+    saveForm({ id, formElements });
+    toast.message("Form changes saved locally", { duration: 1000 });
+  }
   if (!id) {
     redirect("/my-forms");
   }
@@ -80,9 +88,9 @@ export function FormBuilder() {
                 <div className="pt-2">
                   <FormEdit />
                   <div className="pt-4 flex items-center justify-between">
-                    {/* <Button variant="ghost" onClick={handleSaveForm}>
+                    <Button variant="ghost" onClick={handleSaveForm}>
                       Save
-                    </Button> */}
+                    </Button>
                     {formElements.length > 1 && (
                       <Button variant="ghost" onClick={resetForm}>
                         Remove All
