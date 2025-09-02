d3491ea0bd0ae915e37bb671b4cb128d39148905
Ali <ali.hussein.pre@gmail.com>
2025-09-02 21:16:42 +0200

    perf: run posthog only in prod environment

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -9,12 +9,15 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-      defaults: "2025-05-24",
-    });
+    const NEXT_PUBLIC_POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+    if (NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+        person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+        defaults: "2025-05-24",
+      });
+    }
   }, []);
 
   return (
