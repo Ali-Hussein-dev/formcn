dc2cf52a5ff97ba764a600fdeb9e8f0e50349f38
Ali <ali.hussein.pre@gmail.com>
2025-09-06 14:35:10 +0200

    refactor: prepare the code for registry

--- apps/web/src/form-builder/components/generated-code/code-viewer.tsx
+++ apps/web/src/form-builder/components/generated-code/code-viewer.tsx
@@ -90,121 +90,68 @@ const installableShadcnComponents: Partial<
   Password: "https://formcn.dev/r/password.json",
   FileUpload: "https://formcn.dev/r/file-upload.json",
 };
+
+const PackagesInstallation = ({
+  list,
+}: {
+  list: { value: string; name: string }[];
+}) => {
+  const [activeTab, setActiveTab] = React.useState("pnpm");
+
+  return (
+    <Tabs
+      defaultValue="pnpm"
+      value={activeTab}
+      onValueChange={setActiveTab}
+      className="w-full mt-2 rounded-md bg-accent py-2 px-1"
+    >
+      <div className="flex justify-between border-b border-dashed pb-1 px-2">
+        <TabsList>
+          <IoTerminal className="mr-1.5" />
+          {list.map((item) => (
+            <TabsTrigger key={item.name} value={item.name}>
+              {item.name}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <CopyButton
+          text={list.find((item) => item.value === activeTab)?.value || ""}
+        />
+      </div>
+      {list.map((item) => (
+        <TabsContent key={item.name} value={item.name}>
+          <div>
+            <pre className="px-3 py-2 text-accent-foreground/80 text-wrap">
+              {item.value}
+            </pre>
+          </div>
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+};
+
 //======================================
-export function CodeBlockPackagesInstallation() {
-  const formElements = useFormBuilderStore((s) => s.formElements);
-  const isMS = useFormBuilderStore((s) => s.isMS);
-  const [activeTab, setActiveTab] = React.useState("bun"); // Default to bun since project uses it
-  const processedFormElements = isMS
-    ? flattenFormSteps(formElements as FormStep[])
-    : formElements;
-  const formElementTypes = (processedFormElements.flat() as FormElement[])
-    .filter((el) => !el.static)
-    .map((el) => el.fieldType)
-    .map((str) => installableShadcnComponents[str])
-    .filter((str) => str && str.length > 0);
-
-  const packagesSet = new Set(formElementTypes);
-  const packages = Array.from(packagesSet).join(" ");
-  const otherPackages = "react-hook-form zod @hookform/resolvers motion";
-  const tabsData = [
-    {
-      value: "pnpm",
-      shadcn: `pnpm add shadcn@latest add ${packages}`,
-      base: `pnpm add ${otherPackages}`,
-    },
-    {
-      value: "npm",
-      shadcn: `npx shadcn@latest add ${packages}`,
-      base: `npx i ${otherPackages}`,
-    },
-    {
-      value: "yarn",
-      shadcn: `yarn shadcn@latest add ${packages}`,
-      base: `yarn add ${otherPackages}`,
-    },
-    {
-      value: "bun",
-      shadcn: `bunx --bun shadcn@latest add ${packages}`,
-      base: `bunx --bun add ${otherPackages}`,
-    },
-  ];
-
+export function CodeBlockPackagesInstallation({
+  depenedenciesTabs,
+  registryDependeciesTabs,
+}: {
+  depenedenciesTabs: { value: string; name: string }[];
+  registryDependeciesTabs: { value: string; name: string }[];
+}) {
   return (
     <div className="w-full py-5 max-w-full">
       <h2 className="font-sembold text-start">Install base packages</h2>
-      <Tabs
-        defaultValue="bun"
-        value={activeTab}
-        onValueChange={setActiveTab}
-        className="w-full mt-2 rounded-md bg-accent py-2 px-1"
-      >
-        <div className="flex justify-between border-b border-dashed pb-1 px-2">
-          <TabsList>
-            <IoTerminal className="mr-1.5" />
-            {tabsData.map((item) => (
-              <TabsTrigger key={item.value} value={item.value}>
-                {item.value}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          <CopyButton
-            text={tabsData.find((item) => item.value === activeTab)?.base || ""}
-          />
-        </div>
-        {tabsData.map((item) => (
-          <TabsContent key={item.value} value={item.value}>
-            <div>
-              <pre className="px-3 py-2 text-accent-foreground/80 text-wrap">
-                {item.base}
-              </pre>
-            </div>
-          </TabsContent>
-        ))}
-      </Tabs>
+      <PackagesInstallation list={depenedenciesTabs} />
       <h2 className="font-sembold text-start mt-4">
         Install required form components
       </h2>
-      <Tabs
-        defaultValue="bun"
-        value={activeTab}
-        onValueChange={setActiveTab}
-        className="w-full mt-2 rounded-md bg-accent py-2 px-1"
-      >
-        <div className="flex justify-between border-b border-dashed pb-1 px-2">
-          <TabsList>
-            <IoTerminal className="mr-1.5" />
-            {tabsData.map((item) => (
-              <TabsTrigger key={item.value} value={item.value}>
-                {item.value}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          <CopyButton
-            text={
-              tabsData.find((item) => item.value === activeTab)?.shadcn || ""
-            }
-          />
-        </div>
-        {tabsData.map((item) => (
-          <TabsContent key={item.value} value={item.value}>
-            <pre className="px-3 py-2 text-accent-foreground/80 text-wrap">
-              {item.shadcn}
-            </pre>
-          </TabsContent>
-        ))}
-      </Tabs>
+      <PackagesInstallation list={registryDependeciesTabs} />
     </div>
   );
 }
-const CodeBlockTSX = () => {
-  const formElements = useFormBuilderStore((s) => s.formElements);
-  const isMS = useFormBuilderStore((s) => s.isMS);
-  const generatedCode = generateFormCode({
-    formElements: formElements as FormElementOrList[],
-    isMS,
-  });
-  const formattedCode = generatedCode.map((item) => ({
+const CodeBlockTSX = ({ code }: { code: { file: string; code: string }[] }) => {
+  const formattedCode = code.map((item) => ({
     ...item,
     code: formatCode(item.code),
   }));
@@ -218,16 +165,8 @@ const CodeBlockTSX = () => {
     </div>
   );
 };
-const CodeBlockZodSchema = () => {
-  const formElements = useFormBuilderStore((s) => s.formElements);
-  const isMS = useFormBuilderStore((s) => s.isMS);
-  const parsedFormElements = isMS
-    ? flattenFormSteps(formElements as FormStep[])
-    : formElements.flat();
-  const generatedCode = genFormZodSchemaCode(
-    parsedFormElements as FormElement[]
-  );
-  const formattedCode = formatCode(generatedCode);
+const CodeBlockZodSchema = ({ code }: { code: string }) => {
+  const formattedCode = formatCode(code);
   return (
     <div className="relative max-w-full">
       <Wrapper title="schema.ts" language="typescript">
@@ -236,10 +175,85 @@ const CodeBlockZodSchema = () => {
     </div>
   );
 };
+const useGenerateCode = () => {
+  const formElements = useFormBuilderStore((s) => s.formElements);
+  const meta = useFormBuilderStore((s) => s.meta);
+  const isMS = useFormBuilderStore((s) => s.isMS);
+  const tsx = generateFormCode({
+    formElements: formElements as FormElementOrList[],
+    isMS,
+  });
+  const parsedFormElements = isMS
+    ? flattenFormSteps(formElements as FormStep[])
+    : formElements.flat();
+  const zodSchema = genFormZodSchemaCode(parsedFormElements as FormElement[]);
+  const serverAction = generateServerActionCode();
+  const processedFormElements = isMS
+    ? flattenFormSteps(formElements as FormStep[])
+    : formElements;
+  const formElementTypes = (processedFormElements.flat() as FormElement[])
+    .filter((el) => !el.static)
+    .map((el) => el.fieldType)
+    .map((str) => installableShadcnComponents[str])
+    .filter((str) => str && str.length > 0);
 
-const CodeBlockServerAction = () => {
-  const generatedCode = generateServerActionCode();
-  const formattedCode = generatedCode.map((item) => ({
+  const packagesSet = new Set(formElementTypes);
+  const registryDependencies = Array.from(packagesSet).join(" ");
+  const dependencies = "react-hook-form zod @hookform/resolvers motion";
+  const dependenciesTabs = [
+    {
+      name: "pnpm",
+      value: `pnpm add ${dependencies}`,
+    },
+    {
+      name: "npm",
+      value: `npx i ${dependencies}`,
+    },
+    {
+      name: "yarn",
+      value: `yarn add ${dependencies}`,
+    },
+    {
+      name: "bun",
+      value: `bunx --bun add ${dependencies}`,
+    },
+  ];
+  const registryDependenciesTabs = [
+    {
+      name: "pnpm",
+      value: `pnpm add ${registryDependencies}`,
+    },
+    {
+      name: "npm",
+      value: `npx i ${registryDependencies}`,
+    },
+    {
+      name: "yarn",
+      value: `yarn add ${registryDependencies}`,
+    },
+    {
+      name: "bun",
+      value: `bunx --bun add ${registryDependencies}`,
+    },
+  ];
+
+  return {
+    tsx,
+    zodSchema,
+    serverAction,
+    meta,
+    registryDependencies,
+    dependencies,
+    registryDependenciesTabs,
+    dependenciesTabs,
+  };
+};
+const CodeBlockServerAction = ({
+  code,
+}: {
+  code: { file: string; code: string }[];
+}) => {
+  const formattedCode = code.map((item) => ({
     ...item,
     code: formatCode(item.code),
   }));
@@ -257,6 +271,13 @@ const CodeBlockServerAction = () => {
 //======================================
 export function GeneratedFormCodeViewer() {
   const formElements = useFormBuilderStore((s) => s.formElements);
+  const {
+    serverAction,
+    zodSchema,
+    tsx,
+    dependenciesTabs,
+    registryDependenciesTabs,
+  } = useGenerateCode();
   if (formElements.length < 1) {
     return (
       <Placeholder>
@@ -265,26 +286,31 @@ export function GeneratedFormCodeViewer() {
     );
   }
   return (
-    <Tabs defaultValue="tsx" className="w-full min-w-full">
-      <div className="flex justify-between">
-        <TabsList>
-          <TabsTrigger value="tsx">TSX</TabsTrigger>
-          <TabsTrigger value="schema">Schema</TabsTrigger>
-          <TabsTrigger value="server-action">Server action</TabsTrigger>
-        </TabsList>
-        <GeneratedCodeInfoCard />
-      </div>
-      <TabsContent value="tsx" tabIndex={-1}>
-        <CodeBlockTSX />
-        <div className="border-t border-dashed w-full mt-6" />
-        <CodeBlockPackagesInstallation />
-      </TabsContent>
-      <TabsContent value="schema" tabIndex={-1}>
-        <CodeBlockZodSchema />
-      </TabsContent>
-      <TabsContent value="server-action" tabIndex={-1}>
-        <CodeBlockServerAction />
-      </TabsContent>
-    </Tabs>
+    <div>
+      <Tabs defaultValue="tsx" className="w-full min-w-full">
+        <div className="flex justify-between">
+          <TabsList>
+            <TabsTrigger value="tsx">TSX</TabsTrigger>
+            <TabsTrigger value="schema">Schema</TabsTrigger>
+            <TabsTrigger value="server-action">Server action</TabsTrigger>
+          </TabsList>
+          <GeneratedCodeInfoCard />
+        </div>
+        <TabsContent value="tsx" tabIndex={-1}>
+          <CodeBlockTSX code={tsx} />
+          <div className="border-t border-dashed w-full mt-6" />
+          <CodeBlockPackagesInstallation
+            depenedenciesTabs={dependenciesTabs}
+            registryDependeciesTabs={registryDependenciesTabs}
+          />
+        </TabsContent>
+        <TabsContent value="schema" tabIndex={-1}>
+          <CodeBlockZodSchema code={zodSchema} />
+        </TabsContent>
+        <TabsContent value="server-action" tabIndex={-1}>
+          <CodeBlockServerAction code={serverAction} />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 }
