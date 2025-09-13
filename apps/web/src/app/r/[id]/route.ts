549a9333a75e8980e8b97f83984a74ee47979a6b
Ali <ali.hussein.pre@gmail.com>
2025-09-13 12:49:17 +0200

    refactor: use one route to create/get runtime registries

--- apps/web/src/app/r/[id]/route.ts
+++ apps/web/src/app/r/[id]/route.ts
@@ -1,5 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
+import path from "path";
+import fs from "fs";
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
@@ -38,5 +40,59 @@ export const GET = async (
       status: 500,
       headers: responseHeaders,
     });
+  }
+};
+
+
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const body = await req.json();
+    const { registryDependencies, dependencies, files, name } = body;
+    const { id: key } = await params;
+    const registry = {
+      $schema: "https://ui.shadcn.com/schema/registry.json",
+      homepage: "https://formcn.dev",
+      author: "formcn (https://formcn.dev)",
+      name,
+      dependencies,
+      registryDependencies,
+      type: "registry:block",
+      files,
+    };
+    const isDev = process.env.NODE_ENV === "development";
+    if (isDev) {
+      // Create public/r directory if it doesn't exist
+      const publicDir = path.join(process.cwd(), "public", "r");
+      if (!fs.existsSync(publicDir)) {
+        fs.mkdirSync(publicDir, { recursive: true });
+      }
+      const registryPath = path.join(publicDir, `${key}.json`);
+      fs.writeFileSync(registryPath, JSON.stringify(registry, null, 2));
+
+      // console.log(`Registry file generated at ${registryPath}`);
+
+      return NextResponse.json({
+        data: {
+          id: `http://localhost:3000/r/${key}.json`,
+        },
+        error: null,
+      });
+    } else {
+      await redis.set(key, JSON.stringify(registry), {
+        ex: 60 * 60 * 24, // 1 day
+      });
+      return NextResponse.json({
+        data: {
+          id: `@formcn/${key}`,
+        },
+        error: null,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ data: null, error });
   }
 };
