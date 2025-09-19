cb4be777894ab2526efbac60cf2b8759ea357255
Ali <ali.hussein.pre@gmail.com>
2025-09-19 19:01:49 +0200

    fix: handle stretching issue with step-container

--- apps/web/src/form-builder/components/edit/step-container.tsx
+++ apps/web/src/form-builder/components/edit/step-container.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import useFormBuilderStore from "@/form-builder/hooks/use-form-builder-store";
 import { MdAdd, MdDelete } from "react-icons/md";
 import { FormElementsStepDropdown } from "@/form-builder/components/edit/form-elements-dropdown";
+import { motion } from "motion/react";
 //======================================
 export function StepContainer({
   children,
@@ -12,15 +13,15 @@ export function StepContainer({
 }) {
   const { addFormStep, removeFormStep, formElements } = useFormBuilderStore();
   return (
-    <div className="rounded-lg px-3 md:px-4 md:py-5 py-4 border-dashed border bg-muted">
-      <div className="flex items-center justify-between mb-3">
+    <div className="rounded-lg px-3 md:px-4 lg:px-5 md:py-5 py-4 border border-dashed bg-muted">
+      <motion.div layout className="flex items-center justify-between pb-4">
         <FormElementsStepDropdown stepIndex={stepIndex} />
         <div className="text-muted-foreground center font-medium pr-2">
           Step {stepIndex + 1}
         </div>
-      </div>
+      </motion.div>
       <div className="space-y-3">{children}</div>
-      <div className="flex items-center justify-end px-2 pt-4">
+      <motion.div layout className="flex items-center justify-end px-2 pt-4">
         <div className="flex items-center justify-end gap-3">
           {formElements.length > 1 && (
             <Button
@@ -42,7 +43,7 @@ export function StepContainer({
             <span>Add Step</span>
           </Button>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
